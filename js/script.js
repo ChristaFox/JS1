180b8b625abbf8c375f915759da8d4c86726e22d
ChristaFox <christa.r.fox@me.com>
2018-04-09 21:00:54 -0700

    script stuff

---
+++ js/script.js
@@ -0,0 +1,3 @@
+var myHistory = 'I joined the cohort, and we have to coteach courses. This is my first solo one, but as a cohort we are teaching assistants for the West Hills course.';
+
+console.log(myHistory);
